888c5714cbc1bc211fb7c70474234461810516a9
Karla Rosas <k2.rosas.v@gmail.com>
2021-03-09 13:45:31 -0600

    Numbers section

---
+++ numbers_exercises/birthday.py
@@ -0,0 +1,15 @@
+# str() to represent non-string values as strings
+age = 25
+message = "Happy " + str(age) + "th Birthday!"
+print(message)
+
+# 2-8 Number Eight: Write addition, subtraction, multiplication and division that result on 8
+print(7 + 1)
+print(10 - 2)
+print(4 * 2)
+print(16 / 2)
+
+# 2-9 Favorite Number: Store and number then create a message.
+number = 4
+message = "My favorite number is " + str(number)
+print(message)
